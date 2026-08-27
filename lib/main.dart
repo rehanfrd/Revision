08518eac7bdc5872142ffd79d21e034561096ec7
rehanfrd <rehanbhai8271@gmail.com>
2026-08-27 22:48:19 +0530

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'screens/input_page.dart';
+import 'screens/book_view.dart';
+
+void main() {
+  runApp(VocabRevisionApp());
+}
+
+class VocabRevisionApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Daily Revision Book',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.brown,
+        scaffoldBackgroundColor: Color(0xFFF4ECD8), // Sepia book color
+        fontFamily: 'Georgia', // Classic book serif font
+      ),
+      home: BookViewScreen(),
+    );
+  }
+}
